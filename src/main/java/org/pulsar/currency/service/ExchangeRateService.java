5be020f311ce304f20b83461658db4a29158b055
pulsar <pulsarmn@yandex.ru>
2025-12-03 11:05:58 +0300

    feat: create an ExchangeRateService class and implement getAll() method

---
+++ src/main/java/org/pulsar/currency/service/ExchangeRateService.java
@@ -0,0 +1,44 @@
+package org.pulsar.currency.service;
+
+import org.pulsar.currency.dao.ExchangeRateDao;
+import org.pulsar.currency.dto.CurrencyResponse;
+import org.pulsar.currency.dto.ExchangeRateResponse;
+import org.pulsar.currency.model.Currency;
+import org.pulsar.currency.model.ExchangeRate;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class ExchangeRateService {
+
+    private final ExchangeRateDao exchangeRateDao;
+
+    public ExchangeRateService(ExchangeRateDao exchangeRateDao) {
+        this.exchangeRateDao = exchangeRateDao;
+    }
+
+    public List<ExchangeRateResponse> getAll() {
+        return exchangeRateDao.findAll()
+                .stream()
+                .map(this::mapToResponse)
+                .collect(Collectors.toList());
+    }
+
+    private ExchangeRateResponse mapToResponse(ExchangeRate exchangeRate) {
+        return ExchangeRateResponse.builder()
+                .id(exchangeRate.getId())
+                .baseCurrency(mapToCurrencyResponse(exchangeRate.getBaseCurrency()))
+                .targetCurrency(mapToCurrencyResponse(exchangeRate.getTargetCurrency()))
+                .rate(exchangeRate.getRate())
+                .build();
+    }
+
+    private CurrencyResponse mapToCurrencyResponse(Currency currency) {
+        return CurrencyResponse.builder()
+                .id(currency.getId().toString())
+                .code(currency.getCode())
+                .name(currency.getFullName())
+                .sign(currency.getSign())
+                .build();
+    }
+}
