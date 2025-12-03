dbb8f61f105f1e48ae41b72da8a5984ad041e6e4
pulsar <pulsarmn@yandex.ru>
2025-12-03 10:01:58 +0300

    feat: implement 2 methods in the CurrencyService class
    implemented methods:
    public:
     - create()
     - isExists()
    private:
     - isInvalid()
     - mapToCurrency()

--- src/main/java/org/pulsar/currency/service/CurrencyService.java
+++ src/main/java/org/pulsar/currency/service/CurrencyService.java
@@ -1,12 +1,15 @@
 package org.pulsar.currency.service;
 
 import org.pulsar.currency.dao.CurrencyDao;
+import org.pulsar.currency.dto.CurrencyCreateRequest;
 import org.pulsar.currency.dto.CurrencyResponse;
+import org.pulsar.currency.exception.CurrencyAlreadyExistsException;
 import org.pulsar.currency.exception.CurrencyNotFoundException;
 import org.pulsar.currency.model.Currency;
 import org.pulsar.currency.util.StringUtils;
 
 import java.util.List;
+import java.util.UUID;
 import java.util.stream.Collectors;
 
 public class CurrencyService {
@@ -32,6 +35,42 @@ public class CurrencyService {
         return currencyDao.findByCode(currencyCode)
                 .map(this::mapToResponse)
                 .orElseThrow(CurrencyNotFoundException::new);
+    }
+
+    public CurrencyResponse create(CurrencyCreateRequest createRequest) {
+        if (isInvalid(createRequest)) {
+            throw new IllegalArgumentException("Invalid CurrencyCreateRequest");
+        } else if (isExists(createRequest.code())) {
+            throw new CurrencyAlreadyExistsException();
+        }
+
+        Currency currency = mapToCurrency(createRequest);
+        currencyDao.save(currency);
+
+        return mapToResponse(currency);
+    }
+
+    public boolean isExists(String currencyCode) {
+        return currencyDao.findByCode(currencyCode).isPresent();
+    }
+
+    private boolean isInvalid(CurrencyCreateRequest createRequest) {
+        if (createRequest == null
+                || StringUtils.isNullOrBlank(createRequest.code())
+                || StringUtils.isNullOrBlank(createRequest.name())
+                || StringUtils.isNullOrBlank(createRequest.sign())) {
+            return true;
+        }
+        return createRequest.code().length() != 3;
+    }
+
+    private Currency mapToCurrency(CurrencyCreateRequest createRequest) {
+        return Currency.builder()
+                .id(UUID.randomUUID())
+                .code(createRequest.code())
+                .fullName(createRequest.name())
+                .sign(createRequest.sign())
+                .build();
     }
 
     private CurrencyResponse mapToResponse(Currency currency) {
