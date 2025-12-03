34b56c0289b8208a138bb09be8b1024839682c0a
pulsar <pulsarmn@yandex.ru>
2025-12-03 11:06:23 +0300

    feat: add ExchangeRateService object to ServletContext

--- src/main/java/org/pulsar/currency/listener/StartupListener.java
+++ src/main/java/org/pulsar/currency/listener/StartupListener.java
@@ -6,7 +6,9 @@ import jakarta.servlet.ServletContextListener;
 import jakarta.servlet.annotation.WebListener;
 import org.pulsar.currency.DataSourceFactory;
 import org.pulsar.currency.dao.CurrencyDao;
+import org.pulsar.currency.dao.ExchangeRateDao;
 import org.pulsar.currency.service.CurrencyService;
+import org.pulsar.currency.service.ExchangeRateService;
 import tools.jackson.databind.ObjectMapper;
 
 import javax.sql.DataSource;
@@ -23,6 +25,10 @@ public class StartupListener implements ServletContextListener {
         CurrencyDao currencyDao = new CurrencyDao(dataSource);
         CurrencyService currencyService = new CurrencyService(currencyDao);
         servletContext.setAttribute("currencyService", currencyService);
+
+        ExchangeRateDao exchangeRateDao = new ExchangeRateDao(dataSource);
+        ExchangeRateService exchangeRateService = new ExchangeRateService(exchangeRateDao);
+        servletContext.setAttribute("exchangeRateService", exchangeRateService);
 
         ObjectMapper objectMapper = new ObjectMapper();
         servletContext.setAttribute("objectMapper", objectMapper);
