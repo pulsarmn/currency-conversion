97fe571b949240f7e3215018a6eb274e2e66364c
pulsar <pulsarmn@yandex.ru>
2025-12-04 10:21:28 +0300

    feat: integrate CurrencyService with CurrencyMapper

--- src/main/java/org/pulsar/currency/service/CurrencyService.java
+++ src/main/java/org/pulsar/currency/service/CurrencyService.java
@@ -5,25 +5,27 @@ import org.pulsar.currency.dto.currency.CurrencyCreateRequest;
 import org.pulsar.currency.dto.currency.CurrencyResponse;
 import org.pulsar.currency.exception.currency.CurrencyAlreadyExistsException;
 import org.pulsar.currency.exception.currency.CurrencyNotFoundException;
+import org.pulsar.currency.mapper.CurrencyMapper;
 import org.pulsar.currency.model.Currency;
 import org.pulsar.currency.util.StringUtils;
 
 import java.util.List;
-import java.util.UUID;
 import java.util.stream.Collectors;
 
 public class CurrencyService {
 
     private final CurrencyDao currencyDao;
+    private final CurrencyMapper currencyMapper;
 
-    public CurrencyService(CurrencyDao currencyDao) {
+    public CurrencyService(CurrencyDao currencyDao, CurrencyMapper currencyMapper) {
         this.currencyDao = currencyDao;
+        this.currencyMapper = currencyMapper;
     }
 
     public List<CurrencyResponse> getAll() {
         return currencyDao.findAll()
                 .stream()
-                .map(this::mapToResponse)
+                .map(currencyMapper::mapToResponse)
                 .collect(Collectors.toList());
     }
 
@@ -33,7 +35,7 @@ public class CurrencyService {
         }
 
         return currencyDao.findByCode(currencyCode)
-                .map(this::mapToResponse)
+                .map(currencyMapper::mapToResponse)
                 .orElseThrow(CurrencyNotFoundException::new);
     }
 
@@ -44,10 +46,10 @@ public class CurrencyService {
             throw new CurrencyAlreadyExistsException();
         }
 
-        Currency currency = mapToCurrency(createRequest);
+        Currency currency = currencyMapper.map(createRequest);
         currencyDao.save(currency);
 
-        return mapToResponse(currency);
+        return currencyMapper.mapToResponse(currency);
     }
 
     public boolean isExists(String currencyCode) {
@@ -62,23 +64,5 @@ public class CurrencyService {
             return true;
         }
         return createRequest.code().length() != 3;
-    }
-
-    private Currency mapToCurrency(CurrencyCreateRequest createRequest) {
-        return Currency.builder()
-                .id(UUID.randomUUID())
-                .code(createRequest.code())
-                .fullName(createRequest.name())
-                .sign(createRequest.sign())
-                .build();
-    }
-
-    private CurrencyResponse mapToResponse(Currency currency) {
-        return CurrencyResponse.builder()
-                .id(currency.getId().toString())
-                .code(currency.getCode())
-                .name(currency.getFullName())
-                .sign(currency.getSign())
-                .build();
     }
 }
